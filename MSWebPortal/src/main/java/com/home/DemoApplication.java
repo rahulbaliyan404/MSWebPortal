598a1100ded78e2fd753df72cd0e59d7dee8061d
rahulbaliyan <rahulbaliyan404@gmail.com>
2020-11-23 19:56:08 +0530

    new line added.

---
+++ MSWebPortal/src/main/java/com/home/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	//main method.
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
